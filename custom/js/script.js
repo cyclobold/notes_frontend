47a5c9829abb7b4f856163c440602d9c4d360236
Olu <theoafactor@gmail.com>
2022-04-01 12:00:58 +0100

    Added get-posts

--- custom/js/script.js
+++ custom/js/script.js
@@ -63,3 +63,40 @@ createPostForm.addEventListener("submit", function(e){
 
 
 })
+
+
+
+window.onload = function(){
+
+  axios.get("http://localhost:4000/get-posts").then((feedback) =>{
+    let postsContents =  document.querySelector("#posts-contents");
+        result = feedback.data.data;
+
+        for(let i = 0; i < result.length; i++){
+          code = `<div class="col-md-6">
+            <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div class="col p-4 d-flex flex-column position-static">
+                <strong class="d-inline-block mb-2 text-primary">World</strong>
+                <h3 class="mb-0">${result[i].post_title}</h3>
+                <div class="mb-1 text-muted">Nov 12</div>
+                <p class="card-text mb-auto">${result[i].post_summary}</p>
+                <a href="#" class="stretched-link">Continue reading</a>
+              </div>
+              <div class="col-auto d-none d-lg-block">
+                <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+      
+              </div>
+            </div>
+          </div>`;
+
+          postsContents.innerHTML += code;
+
+
+        }
+    
+    console.log(feedback);
+
+
+  })
+
+}
